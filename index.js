8bc1afb92e757504bdad8aabca098676d135f32b
George Ananda <georgeananda@gmail.com>
2021-03-26 12:59:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+
+const port = process.env.PORT || 8080;
+const isTest = process.env.NODE_ENV === 'test';
+
+const app = express();
+
+const server = app.listen(port, () => console.log(`Listening at port ${port}, environment: ${process.env.NODE_ENV}`));
+
+app.use(express.json());
+if (!isTest) app.use(morgan('tiny'));
+
+module.exports = { app, server };
