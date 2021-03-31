95f0a38bf2e4889837e9320c20a63592dadccb35
George Ananda <georgeananda@gmail.com>
2021-03-31 16:23:25 +0700

    Add rich environment variable logging

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const name = process.env.npm_package_name;
+const version = process.env.npm_package_version;
+const environment = process.env.NODE_ENV;
 const port = process.env.PORT || 8080;
 const isTest = process.env.NODE_ENV === 'test';
 
 const app = express();
 
-const server = app.listen(port, () => console.log(`Listening at port ${port}, environment: ${process.env.NODE_ENV}`));
+const server = app.listen(port, () => console.log(`name ${name}  v${version}. environment ${process.env.NODE_ENV}. port ${port}`));
 
 app.use(express.json());
 if (!isTest) app.use(morgan('tiny'));
